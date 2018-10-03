d73f10435d78420e391223af4c438f561c7db002
8-shen <664905885@qq.com>
2018-10-03 09:38:34 +0800

    add mysql plugin

--- server/lib/set-context.js
+++ server/lib/set-context.js
@@ -8,20 +8,21 @@ const logger = require('./vendor/log4js');
 const webpackEntryConf = require(path.resolve('./webapck.entry.conf'));
 const Mysql = require('./utils/mysql-driver');
 
-
+let argv = process.argv.splice(2);
+let env = process.env.NODE_ENV = argv[0] !== 'production' ? 'development' : 'production';
+let appConfig = Object.assign({},
+    config,
+    config[env], {
+        env: env
+    }, {
+        urlLocalesRegExp: ''
+    }
+);
 
 /**
  * 基础上下文配置(静态)
  */
 function appContextConfig(app) {
-    let argv = process.argv.splice(2);
-    let env = process.env.NODE_ENV = argv[0] !== 'production' ? 'development' : 'production';
-    let appConfig = Object.assign({},
-        config,
-        config[env],
-        {env: env},
-        {urlLocalesRegExp: ''}
-    );
     let locales = config.locales;
     //多语言前缀
     if (!isEmpty(locales)) {
@@ -67,7 +68,7 @@ function appContextConfig(app) {
  * 包上通用内容到上下文(可变) 比如axios ,某些utils方法....
  */
 function wrapCommonToContext(app) {
-    app.context.mysql = new Mysql(require('../config/index').mysql);
+    app.context.mysql = new Mysql(appConfig.mysql);
     app.context.axios = axios;
     app.context.logger = logger;
 }
